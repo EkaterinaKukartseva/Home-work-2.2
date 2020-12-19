ea1430ef63c7c85b489b8b6d1904fc433b6b44ee
Ekaterina Kukartseva <sos91195@mail.ru>
2020-12-19 22:09:25 +0300

    correction of inaccuracies

--- Home work 2.2/ViewController.swift
+++ Home work 2.2/ViewController.swift
@@ -20,27 +20,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var sliderBlue: UISlider!
     @IBOutlet weak var sliderAlpha: UISlider!
     
-    let numberFormatter: NumberFormatter = {
-        let nf = NumberFormatter()
-        nf.numberStyle = .decimal
-        nf.maximumFractionDigits = 2
-        return nf
-    }()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         colorView.layer.cornerRadius = 10
+        
+        setColorLabel()
+        setColorView()
     }
 
     @IBAction func sliderAction(_ sender: Any) {
-        valueRed.text = numberFormatter.string(from: NSNumber(value: sliderRed.value))
-        valueGreen.text = numberFormatter.string(from: NSNumber(value: sliderGreen.value))
-        valueBlue.text = numberFormatter.string(from: NSNumber(value: sliderBlue.value))
-        valueAlpha.text = numberFormatter.string(from: NSNumber(value: sliderAlpha.value))
-        
+        setColorLabel()
+        setColorView()
+    }
+    
+    func setColorView() {
         colorView.backgroundColor = UIColor(red: CGFloat(sliderRed.value)/255, green: CGFloat(sliderGreen.value/225), blue: CGFloat(sliderBlue.value/225), alpha: CGFloat(sliderAlpha.value))
     }
     
+    func setColorLabel() {
+        valueRed.text = String(Int(sliderRed.value))
+        valueGreen.text = String(Int(sliderGreen.value))
+        valueBlue.text = String(Int(sliderBlue.value))
+        valueAlpha.text = String(format: "%.02f", sliderAlpha.value)
+    }
 }
 
